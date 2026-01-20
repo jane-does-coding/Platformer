c948d31dca10477a0387ef7d5a794b5cb7dbf5f4
Jane <yevheniiasimaka@gmail.com>
2026-01-19 17:50:21 -0700

    added my own art ~2 hours

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 
 def display_score():
-    current_time = int(pygame.time.get_ticks()) - start_time
-    score_surf = test_font.render(f"Score: {current_time//1000}", False, (64, 64, 64))
+    current_time = pygame.time.get_ticks() - start_time
+    score = current_time // 1000
+    score_surf = test_font.render(f"Score: {score}", False, (64, 64, 64))
     score_rect = score_surf.get_rect(center=(400, 50))
-    screen.blit(score_surf, score_rect) 
-    print(current_time)
+    screen.blit(score_surf, score_rect)
+    return score
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
@@ -14,15 +15,22 @@ clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 game_active = False
 start_time = 0
+score = 0
 
-sky_surface = pygame.image.load("graphics/Sky.png").convert()
-ground_surface = pygame.image.load("graphics/ground.png").convert()
+sky_surface = pygame.image.load("graphics/sky2.png").convert()
+sky_surface = pygame.transform.scale(sky_surface, (800, 350))
+
+ground_surface = pygame.image.load("graphics/Ground2.png").convert()
+ground_surface = pygame.transform.scale(ground_surface, (800, 100))
 
 # score_surf = test_font.render("My Game", False, (64, 64, 64))
 # score_rect = score_surf.get_rect(center=(400, 50))
 
-snail_surf = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_surf = pygame.image.load("graphics/snail/Snail.png").convert_alpha()
+snail_surf = pygame.transform.scale(snail_surf, (50, 40))  # adjust size here
 snail_rect = snail_surf.get_rect(bottomright=(600, 300))
+
+# TO DO CHANGE THE SNAIL SIZE
 
 player_surf = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(80,300)) 
@@ -32,6 +40,12 @@ player_gravity = 0
 player_stand = pygame.image.load("graphics/player/player_stand.png").convert_alpha()
 player_stand = pygame.transform.scale2x(player_stand)
 player_stand_rect = player_stand.get_rect(center=(400, 200))
+
+game_name = test_font.render("Pixel Runner", False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center=(400, 80))
+
+game_message = test_font.render("Press space to run", False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center = (400, 340))
 
 while True: 
     for event in pygame.event.get():
@@ -64,7 +78,7 @@ while True:
         # pygame.draw.rect(screen, "#c0e8ec", score_rect)
         # pygame.draw.rect(screen, "#c0e8ec", score_rect, 10)
         # screen.blit(score_surf, score_rect)
-        display_score()
+        score = display_score()
 
         snail_rect.x -= 4
         if snail_rect.right <= 0: snail_rect.left = 800
@@ -82,6 +96,17 @@ while True:
     else:
         screen.fill((94, 129, 162))
         screen.blit(player_stand, player_stand_rect)
+
+        score_message = test_font.render(f"Your score: {score}", False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center = (400, 330))
+
+        screen.blit(game_name, game_name_rect)
+        # screen.blit(game_message, game_message_rect)
+
+        if score == 0:
+            screen.blit(game_message, game_message_rect)
+        else:
+            screen.blit(score_message, score_message_rect)
 
     pygame.display.update()
     clock.tick(60)
